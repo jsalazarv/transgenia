7123a37473e503f64f0c96a5e8db5b9e50d64f2d
Juan Salazar <jsalazarv@Juans-MacBook-Pro.local>
2022-08-08 14:47:34 -0500

    Added base page

---
+++ src/views/Business/BusinessList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-layout="'public-layout'"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class BusinessList extends Vue {}
+</script>
+
+<style lang="scss"></style>
